198a7061de11c1c41fe78c1315fb861062293817
DayBF <barrios.daymara@gmail.com>
2024-06-20 01:05:32 -0300

    Pagina de contacto

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import FormMessage from "./FormMessage";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [name, setName] = useState("")
+  const [mail, setMail] = useState("")
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  const nameRegex = !/^\s/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleName = (e)=>{
+    setName(e.target.value)
+  }
+
+  const handleColor = (e)=> {
+    setMail(e.target.value)
+  }
+
+  const eventHandler =(e)=> {
+    e.preventDefault()
+    if (name.trim().length > 3 && nameRegex.test(name) && emailRegex.test(mail)) {
+      setShow(true);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={eventHandler}>
+        <input onChange={handleName} type="text" placeholder="ingresar nombre" value={name} />
+        <input onChange={handleColor}  type="text" placeholder="ingresa tu correo" value={mail} />
+        <button style={{backgroundColor:"pink", color: "gray"}}>Enviar</button>
       </form>
+      {error && <p style={{color:"red"}}>Por favor verifique su información nuevamente</p>}
+      {show && <FormMessage nombre={name} correo={mail}/>}
     </div>
   );
 };
---
+++ src/Components/FormMessage.jsx
@@ -0,0 +1,10 @@
+export const FormMessage = ({nombre, correo}) => {
+  
+    return (     
+        <div>
+            <p>Gracias {nombre}, te contactaremos cuando antes a tu mail {correo}</p>
+        </div> 
+    )
+  }
+  export default FormMessage;
+  
